11be6e085dafde5d701e777e8a0c46f63d4895e7
Jonathan <jonathanjl99@gmail.com>
2013-07-19 21:17:54 +0200

    Write(Vector3 or QVector3) and Pad extensions for BinaryWriter.

---
+++ Coob/BinaryWriterExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Coob
+{
+    public static class BinaryWriterExtensions
+    {
+        public static void Write(this BinaryWriter writer, Vector3 vec3)
+        {
+            writer.Write(vec3.X);
+            writer.Write(vec3.Y);
+            writer.Write(vec3.Z);
+        }
+
+        public static void Write(this BinaryWriter writer, QVector3 qvec3)
+        {
+            writer.Write(qvec3.X);
+            writer.Write(qvec3.Y);
+            writer.Write(qvec3.Z);
+        }
+
+        public static void Pad(this BinaryWriter writer, int length)
+        {
+            var nullArray = new byte[length];
+            writer.Write(nullArray);
+        }
+    }
+}
