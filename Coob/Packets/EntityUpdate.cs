55d0a064090525f1ca0a0b659e1fa3e9cfbdc297
Jonathan <jonathanjl99@gmail.com>
2013-07-20 12:28:18 +0200

    Commented non working code until perl fixes his shit c;

--- Coob/Packets/EntityUpdate.cs
+++ Coob/Packets/EntityUpdate.cs
@@ -82,26 +82,26 @@ namespace Coob.Packets
 
             public override void Process()
             {
-                Entity.CopyByMask(Changes);
-
-                byte[] compressed;
-
-                using (var ms = new MemoryStream())
-                using (var bw = new BinaryWriter(ms))
-                {
-                    bw.Write(Entity.ID);
-                    bw.Write(Changes.LastBitmask);
-                    Entity.WriteByMask(Changes.LastBitmask, bw);
-
-                    compressed = ZlibHelper.CompressBuffer(ms.ToArray());
-                }
-
-                foreach (var client in Root.Coob.GetClients())
-                {
-                    client.Writer.Write(0);
-                    client.Writer.Write(compressed.Length);
-                    client.Writer.Write(compressed);
-                }
+                //Entity.CopyByMask(Changes);
+                //
+                //byte[] compressed;
+                //
+                //using (var ms = new MemoryStream())
+                //using (var bw = new BinaryWriter(ms))
+                //{
+                //    bw.Write(Entity.ID);
+                //    bw.Write(Changes.LastBitmask);
+                //    Entity.WriteByMask(Changes.LastBitmask, bw);
+                //
+                //    compressed = ZlibHelper.CompressBuffer(ms.ToArray());
+                //}
+                //
+                //foreach (var client in Root.Coob.GetClients())
+                //{
+                //    client.Writer.Write(0);
+                //    client.Writer.Write(compressed.Length);
+                //    client.Writer.Write(compressed);
+                //}
             }
         }
     }
