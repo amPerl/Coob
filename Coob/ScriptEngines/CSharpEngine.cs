fc4af276302f4ff091fcca3f6c0b600189c447f3
PhonicUK <phonicuk@googlemail.com>
2013-07-21 15:32:52 +0100

    Start c# plugin engine, move some files aboot.

---
+++ Coob/ScriptEngines/CSharpEngine.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.CSharp;
+using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Coob.ScriptEngines
+{
+    class CSharpEngine : IScriptHandler
+    {
+        private string source;
+
+        public void SetFunction(string name, Delegate function)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void SetParameter(string name, object value)
+        {
+            //throw new NotImplementedException();
+        }
+
+        CSharpCodeProvider codeProvider;
+        ICodeCompiler icc;
+        Assembly compiledAssembly;
+        Type mainType;
+        Object mainClass;
+
+        public void Initialize()
+        {
+            codeProvider = new CSharpCodeProvider();
+        }
+
+        public void Load(string sourceFile)
+        {
+            source = File.ReadAllText(sourceFile);
+            var res = codeProvider.CompileAssemblyFromSource(
+                new System.CodeDom.Compiler.CompilerParameters()
+                {
+                    GenerateInMemory = true
+                },
+                source
+            );
+
+            compiledAssembly = res.CompiledAssembly;
+
+            mainType = compiledAssembly.GetType("CoobPlugin");
+            mainClass = Activator.CreateInstance(mainType);
+        }
+
+        public void Run()
+        {
+            mainType.GetMethod("main").Invoke(mainClass, new object[]{});
+        }
+
+        public T CallFunction<T>(string functionName, params object[] arguments)
+        {
+            var output = mainType.GetMethod(functionName).Invoke(mainClass, arguments);
+            return (T)output;
+        }
+
+        public void CallFunction(string functionName, params object[] arguments)
+        {
+            mainType.GetMethod(functionName).Invoke(mainClass, arguments);
+        }
+    }
+}
