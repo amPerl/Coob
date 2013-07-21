115aaecd1a22e539843187fc96e5f7b073132dfb
Gareth Higgins <gareth.higgins@ryerson.ca>
2013-07-21 04:12:02 -0400

    Changed tempBuffer initialisation.
    Since tempBuffer is only ever 8 bytes, initialise it when the constructor is called then only write/read the amount of byte needed from/to it.

--- Coob/NetReader.cs
+++ Coob/NetReader.cs
@@ -13,6 +13,7 @@ namespace Coob
             base(stream)
         {
             ns = stream;
+            tempBuffer = new byte[8];
         }
 
         public override byte ReadByte()
@@ -30,35 +31,30 @@ namespace Coob
 
         public override short ReadInt16()
         {
-            tempBuffer = new byte[2];
             ns.Read(tempBuffer, 0, 2);
             return BitConverter.ToInt16(tempBuffer, 0);
         }
 
         public override ushort ReadUInt16()
         {
-            tempBuffer = new byte[2];
             ns.Read(tempBuffer, 0, 2);
             return BitConverter.ToUInt16(tempBuffer, 0);
         }
 
         public override int ReadInt32()
         {
-            tempBuffer = new byte[4];
             ns.Read(tempBuffer, 0, 4);
             return BitConverter.ToInt32(tempBuffer, 0);
         }
 
         public override uint ReadUInt32()
         {
-            tempBuffer = new byte[4];
             ns.Read(tempBuffer, 0, 4);
             return BitConverter.ToUInt32(tempBuffer, 0);
         }
 
         public override float ReadSingle()
         {
-            tempBuffer = new byte[4];
             ns.Read(tempBuffer, 0, 4);
             return BitConverter.ToSingle(tempBuffer, 0);
         }
@@ -85,7 +81,6 @@ namespace Coob
 
         public override long ReadInt64()
         {
-            tempBuffer = new byte[8];
             ns.Read(tempBuffer, 0, 8);
             return BitConverter.ToInt64(tempBuffer, 0);
         }
