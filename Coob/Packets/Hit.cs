f0ce9752f593f4761fad217b4797ebe0a1955e75
Jonathan <jonathanjl99@gmail.com>
2013-07-27 04:40:12 +0200

    Made Hit.cs look a bit better

--- Coob/Packets/Hit.cs
+++ Coob/Packets/Hit.cs
@@ -10,10 +10,8 @@ namespace Coob.Packets
 {
     public partial class Packet
     {
-
         public class Hit : Base
         {
-
             public ulong EntityID;
             public ulong TargetID;
             public float Damage;
@@ -26,15 +24,11 @@ namespace Coob.Packets
             public byte HitType;
             public byte ShowLight;
 
-      
-            public Hit(Client client) : base(client)
-            {
-                 
-            }
+            public Hit(Client client) : base(client) { }
 
             public static Base Parse(Client client, Coob coob)
             {
-                Hit hit = new Hit(client);
+                var hit = new Hit(client);
 
                 hit.EntityID = client.Reader.ReadUInt64();
                 hit.TargetID = client.Reader.ReadUInt64();
@@ -43,34 +37,24 @@ namespace Coob.Packets
                 client.Reader.ReadBytes(3);
                 hit.StunDuration = client.Reader.ReadUInt32();
                 hit.Something8 = client.Reader.ReadUInt32();
-                hit.Position = new QVector3
-                   {
-                       X = client.Reader.ReadInt64(),
-                       Y = client.Reader.ReadInt64(),
-                       Z = client.Reader.ReadInt64()
-                   };
-                hit.HitDirection = new Vector3
-                   {
-                       X = client.Reader.ReadSingle(),
-                       Y = client.Reader.ReadSingle(),
-                       Z = client.Reader.ReadSingle()
-                   };
+                hit.Position = client.Reader.ReadQVector3();
+                hit.HitDirection = client.Reader.ReadVector3();
                 hit.Skill = client.Reader.ReadByte();
                 hit.HitType = client.Reader.ReadByte();
                 hit.ShowLight = client.Reader.ReadByte();
                 client.Reader.ReadBytes(1);
                 return hit;
             }
-        
+
 
             public override bool CallScript()
             {
-                return false;
+                return true;
             }
 
             public override void Process()
             {
-                
+                // Todo: Process Hit in world.
             }
         }
     }
