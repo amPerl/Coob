e21acf5e3e239be241b0d30490e88464473c102d
Jonathan <jonathanjl99@gmail.com>
2013-07-28 02:19:26 +0200

    Added CommandManager plugin with sample command (that ironically doesn't do anything yet).

---
+++ Content/Plugins/Javascript/CommandManager/main.js
@@ -0,0 +1,105 @@
+CommandManager = function ()
+{
+    this.commands = {};
+    
+    this.AddCommand = function (command)
+    {
+        this.commands[command.command.toString()] = command;
+        LogInfo("Registered command: " + this.commands[command.command.toString()].command);
+    };
+    
+    this.HandleCommand = function (command, client)
+    {
+        command = command.toString();
+        var arguments = command.split(/ /g);
+        var cmdIdentifier = arguments[0].toString();
+        var targetCommand = this.commands[cmdIdentifier];
+        
+        if (targetCommand != undefined)
+        {
+            var argCount = arguments.length - 1; // Subtract 1 because first entry is the command identifier.
+            
+            if (argCount < targetCommand.minArgCount && argCount > 0)
+            {
+                client.SendServerMessage(targetCommand.notEnoughArgumentsString);
+                return true;
+            }
+            else if (argCount <= 0)
+            {
+                client.SendServerMessage(targetCommand.helpString);
+                return true;
+            }
+
+            targetCommand.args = arguments.length > 1 ? arguments.splice(1, arguments.length - 1) : [];
+            targetCommand.HandleCommand(client);
+        
+            return true;
+        }
+
+        return false;
+    };
+
+    LogInfo("[CommandManager] Initialized.");
+};
+
+Command = function (command, helpString, notEnoughArgumentsString, minArgCount, callback)
+{
+    this.command = command;
+    this.helpString = helpString;
+    this.notEnoughArgumentsString = notEnoughArgumentsString;
+    this.HandleCommand = callback;
+    this.args = [];
+    this.minArgCount = minArgCount;
+    
+    this.JoinArgs = function (start, length)
+    {
+        var args = this.args;
+    
+        if (start == undefined)
+            start = 0;
+    
+        if (start < 0)
+            start = 0;
+
+        if (start > args.length)
+            return "";
+    
+        if (length == undefined)
+            length = args.length - start;
+    
+        if (start + length >= args.length)
+            length = args.length - start;
+    
+        var result = "";
+
+        for (var i = start; i < start + length; ++i)
+        {
+            var arg = args[i];
+        
+            result += arg + " ";
+        }
+    
+        result = result.substr(0, result.length -  1); // Remove last space added above.
+
+        return result;
+    };
+};
+
+commandManager = new CommandManager();
+
+// Include commands here
+#include "teleport.js"
+
+AddHook("OnChatMessage", function (args)
+{
+    var message = args.Message.toString();
+    
+    if (message.substr(0, 1) == "/")
+    {
+        args.Canceled = true;
+        if (!commandManager.HandleCommand(message.trim().substring(1), args.Client))
+        {
+            args.Client.SendServerMessage("Unknown command. (" + message + ")");
+        }
+    }
+});
---
+++ Content/Plugins/Javascript/CommandManager/teleport.js
@@ -0,0 +1,46 @@
+﻿commandManager.AddCommand(new Command(
+    "tp",
+    "Teleport to the given coordinates or player.",
+    "Not enough arguments. You need to specify X Y Z or player name.",
+    1,
+    function (client)
+    {
+        var x, y, z;
+        if (this.args.length == 3)
+        {
+            LogInfo("Parsing XYZ for tp command.");
+            x = parseInt(this.args[0]);
+            y = parseInt(this.args[1]);
+            z = parseInt(this.args[2]);
+
+            if (isNaN(x) || isNaN(y) || isNaN(z)) {
+                client.SendServerMessage("Invalid coordinates given.");
+                return;
+            }
+            else
+            {
+                client.SendServerMessage("Teleported to " + x + "," + y + "," + z + ".");
+            }
+        }
+        else
+        {
+            LogInfo("Parsing name for tp command.");
+            var playerName = this.JoinArgs();
+            LogInfo("Name: " + playerName);
+            var target = coob.GetClient(playerName);
+
+            if (target == null) {
+                client.SendServerMessage("There's no player with the name \"" + playerName + "\".");
+                return;
+            }
+
+            var targetPosition = target.Entity.Position;
+            x = targetPosition.X;
+            y = targetPosition.Y;
+            z = targetPosition.Z;
+            client.SendServerMessage("Teleported to " + target.Entity.Name);
+        }
+
+        // Todo: actually change the position. ;f
+    }
+));
